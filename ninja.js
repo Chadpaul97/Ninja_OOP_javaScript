2e286edd6f4f1e97714f69c89c14fd3deb0b772f
Chadpaul97 <chadpaul97@gmail.com>
2022-01-11 18:56:46 -0600

    added new super class

--- ninja.js
+++ ninja.js
@@ -23,7 +23,28 @@ class Ninja{
 
 }
 
+// Extend the Ninja class and create the Sensei class. A Sensei should have 200 Health, 10 speed, and 10 strength by default. In addition, a Sensei should have a new attribute called wisdom, and the default should be 10. Finally, add the speakWisdom() method. speakWisdom() should call the drinkSake() method from the Ninja class, before console.logging a wise message.
+
+class Sensei extends Ninja{
+    constructor(name){
+        super(name, 200, 10 , 10)
+        this.wisdom = 10
+    }
+    speakWisdom(){
+        this.drinkSake();
+        console.log(`${this.name} says. What one programmer can do in one month, two programmers can do in two months.`)
+    }
+}
 
 const ninja1 = new Ninja("Hyabusa");
 console.log(ninja1)
 ninja1.sayName().showStats().drinkSake();
+
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.showStats();
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
